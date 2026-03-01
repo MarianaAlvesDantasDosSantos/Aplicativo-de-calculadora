d773734a7b844ead226a7fd429b2e89ce9416118
MarianaAlvesDantasDosSantos <claricealvessantos82@gmail.com>
2026-03-01 17:07:43 -0300

    Iniciando projeto

---
+++ calculadora/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+
+      <View style={styles.displayContainer}>
+        <Text style={style.displayText} numberOfLines={1}>
+          67
+        </Text>
+      </View>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  displayContainer: {
+    flex: 2.5,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  displayText: {
+    color: '#ffffff',
+    fontSize: 72,
+    fontWeight: '300'
+  }
+});
